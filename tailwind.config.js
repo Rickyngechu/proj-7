e5c5704df445b465213553a2f8de4ab76bef9350
Erick ngechu <ekngechu003@gmail.com>
2023-10-07 23:41:16 +0300

    update

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {},
+    screens: {
+      sm: "375px",
+      xl: "1440px",
+    },
+    colors: {
+      "Light-red": "hsl(356, 100%, 66%)",
+      "Very-light-red": "hsl(355, 100%, 74%)",
+      "Very-dark-blue": "hsl(208, 49%, 24%)",
+
+      White: " hsl(0, 0%, 100%)",
+      "Grayish-blue": " hsl(240, 2%, 79%)",
+      "Verydark-grayish-blue": " hsl(207, 13%, 34%)",
+      "Verydark-black-blue": "hsl(240, 10%, 16%)",
+
+      "Very-light-red": "hsl(13, 100%, 72%)",
+      "Light-red": "hsl(353, 100%, 62%)",
+
+      "Verydark-gray-blue": "hsl(237, 17%, 21%)",
+      "Verydark-desaturated-blue": "hsl(237, 23%, 32%)",
+    },
+    fontFamily: {
+      sans: ["Overpass", "Ubuntu", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
